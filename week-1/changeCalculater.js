8f4dd13a8b81fe70356930681f0b0c9b63925bf2
Harlow07 <harlowgrind@gmail.com>
2024-04-04 13:37:10 +0100

    create calculate

---
+++ week-1/changeCalculater.js
@@ -0,0 +1,17 @@
+// INPUT
+let price = prompt ("Type in the price of the items:");
+price = Number(price);
+// alert(typeof(price))m
+
+let moneyPaid = prompt ("Type in the money paid:");
+moneyPaid = Number(moneyPaid);
+
+// PROCESSING
+let change = moneyPaid - price;
+
+// OUTPUT
+// If the price of the items is (price) and 
+// the money paid is (moneyPaid) then
+// the change is (change)
+alert("If the price of the item is N" + price + " the money paid is N" +moneyPaid + 
+", the change is N" + change);
--- week-1/output.js
+++ week-1/output.js
@@ -1,1 +1,9 @@
 console.log("Hello !")
+
+console.log(8 * null)
+console.log("5" -1)
+console.log("5" +1)
+console.log("five" +4)
+
+console.log("His name is Harlow")
+console.log('His name is "Harlow"')
