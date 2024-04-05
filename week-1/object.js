0840fe52248678b746a523f84b76cb10cb5984c1
Harlow07 <harlowgrind@gmail.com>
2024-04-05 13:03:36 +0100

    Use object ahead

---
+++ week-1/object.js
@@ -0,0 +1,10 @@
+const car = {
+    brand: "Honda",
+    type:  "SUV",
+    model: "Crosstour",
+    cylinder: "6",
+    isFairlyUsed: true,
+    color: "White",
+}
+
+document.write("<p>" + car.brand + "</p>");
