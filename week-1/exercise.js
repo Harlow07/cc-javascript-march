27ab503fe2d2ff7a9ee4a639e8b64a9d44e472e2
Harlow07 <harlowgrind@gmail.com>
2024-04-08 12:49:02 +0100

    Return the Number of an Array

---
+++ week-1/exercise.js
@@ -0,0 +1,21 @@
+// The sum of two numbers
+
+// INPUT
+/*let firstNumber = prompt ("Type in the first number");
+firstNumber = Number(firstNumber);
+
+let secondNumber = prompt ("Type in the second number");
+secondNumber = Number(secondNumber);
+
+// PROCESSING
+let answer  = firstNumber + secondNumber;
+
+// OUTPUT
+alert(firstNumber + " + " + secondNumber + " = " + answer);
+*/
+
+
+// Convert minutes to seconds 
+
+let arrayOfNumbers = [2, 4, 6, 8, 10];
+console.log(arrayOfNumbers[0]);
