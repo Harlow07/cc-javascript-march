d3d5cbd22dc3437cee944ca7d3899a2bfae7379b
Harlow07 <harlowgrind@gmail.com>
2024-04-15 13:46:17 +0100

    week 3 basics of function

---
+++ week-1/week-3/functions.js
@@ -0,0 +1,19 @@
+function myGreeting(message){
+    return console.log (message);
+}
+
+myGreeting("Hello");
+myGreeting("Today is Monday!");
+
+function addNumbers (firstNumber, secondNumber){
+    return console.log (firstNumber + secondNumber);
+}
+
+addNumbers(3, 4);
+
+let number = 5;
+function increase(){
+    return number + 1;
+}
+
+console.log(increase());
