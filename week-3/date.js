f0c6b0dd8b907209be8d4a5cbfb345f8cec81b9f
Harlow07 <harlowgrind@gmail.com>
2024-04-21 10:19:52 +0100

    writing a code that change background color with Next button

--- week-3/date.js
+++ week-3/date.js
@@ -9,4 +9,36 @@ if (date.getMonth() === 11 && date.getDate() === 24){
 }
 }
 
-console.log(timeForMilkAndCookies(new Date (2013, 0, 23)));
+console.log(timeForMilkAndCookies(new Date (2013, 11, 24)));
+
+
+function isSameNumber (num1, num2){
+    if (num1 === num2){
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+console.log(isSameNumber(2, 7));
+
+
+function calculateNumber (number1, number2){
+    if (number1 + number2 === 30){
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+console.log(calculateNumber (6, 27));
+
+
+
+function findSeven (numbers){
+    if (any 7 in str(num) for num in numbers){
+        return "Boom!"
+    } else {
+        return "There is no 7 in the array"
+    }
+}
